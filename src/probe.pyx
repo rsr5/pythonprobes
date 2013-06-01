91ca85fe54caadf79439d36cd4d8a57f532226f4
Robin Ridler <robin.ridler@gmail.com>
2013-06-01 17:34:00 +0100

    Typos.

--- src/probe.pyx
+++ src/probe.pyx
@@ -6,7 +6,7 @@ def dtrace_probe(*args, **kwargs):
     """
     Fires the static dtrace probe.  You may pass anything up to 5 strings to
     this function and these will be available from dtrace.  For example with
-    ``copyinstr(arg0)``.  A string containing the work ``null`` is returned
+    ``copyinstr(arg0)``.  A string containing the word ``null`` is returned
     for any args that are not passed.  ``null`` was chosen so that it would
     hopefully not clash with any likely strings users would like to pass to
     the probe.

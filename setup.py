5a52d41200a5a553fcd359338e0d695470a6c207
Robin Ridler <robin.ridler@gmail.com>
2013-06-01 15:58:57 +0100

    Added author and name information to setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ from distutils.extension import Extension
 from build_ext import build_ext
 
 setup(
-    ext_modules=[Extension("probe", ["src/probe.c"],
-                           include_dirs=["include"])],
+    name='pythonprobes',
+    version='1.0',
+    description='Static DTrace Probes for Python Applications',
+    author='rsr5',
+    url='https://github.com/rsr5/pythonprobes',
+    ext_modules=[Extension('probe', ['src/probe.c'],
+                           include_dirs=['include'])],
     cmdclass={'build_ext': build_ext}
 )
